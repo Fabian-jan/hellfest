eb9ebfbb6a992dff90f23ccf90040ff24bec409d
Frederic Briand <fbr91144@gmail.com>
2026-05-24 07:42:44 +0200

    relink js and css for artists page

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,3 @@ import * as scripts from './scripts.js';
 import { loadComponent } from './include.js'; 
 
 await loadComponent('#header', '../components/header.html');
-
-import "./artists-cards.js";
