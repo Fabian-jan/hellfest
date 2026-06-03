98b4272593b3bb4a77133805488698b40ed1c1b4
Frederic Briand <fbr91144@gmail.com>
2026-06-03 22:14:30 +0200

    js color picker

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,12 @@
 
 
 import { loadComponent } from './include.js'; 
-import {initBurgerMenu } from './scripts.js';
+import { toggleMenu } from './scripts.js';
+import { changePrimary } from './scripts.js';
 
 await loadComponent('#header', '../components/header.html');
 await loadComponent('#footer', '../components/footer.html');
 
-initBurgerMenu();
+toggleMenu('.navbar-toggle', '.navbar-collapse');
+toggleMenu('.colorz-toggle', '.colorz-picker');
+changePrimary();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,32 @@
-export function initBurgerMenu() {
+// toggle for all menus
+export function toggleMenu(target, menu) {
 
-    const toggle = document.querySelector('.navbar-toggle');
-    const collapse = document.querySelector('.navbar-collapse');
-    console.log(toggle, collapse),
+    const toggle = document.querySelector(target);
+    const collapse = document.querySelector(menu);
     
     toggle.addEventListener('click', () => {
-        collapse.classList.toggle('is-open');
+        collapse.classList.toggle('menu-opened');
     });
     
-    document.querySelectorAll('.navbar-collapse a').forEach(link => {
-        link.addEventListener('click', () => {
-            collapse.classList.remove('is-open');
+}
+
+// rgb to hexadecimal
+function rgbToHex(rgb) {
+    return '#' + rgb.match(/\d+/g)
+        .map(n => parseInt(n).toString(16).padStart(2, '0'))
+        .join('');
+}
+
+// change primary color picker
+export function changePrimary() {
+    const colorItems = document.querySelectorAll('.colorz-picker li');
+    const body = document.querySelector('body');
+    colorItems.forEach(colorItem => {        
+        colorItem.addEventListener('click', () => {
+            const backgroundColor = getComputedStyle(colorItem).backgroundColor;
+            const hexColor = rgbToHex(backgroundColor);
+            console.log(hexColor);
+            body.style.setProperty('--primary', hexColor);
         });
     });
-    
 }
