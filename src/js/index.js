beb38d0af178286d83da1afa688d4ac4caee0724
Frederic Briand <fbr91144@gmail.com>
2026-06-04 12:08:31 +0200

    changes paths

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ import { loadComponent } from './include.js';
 import { toggleMenu } from './scripts.js';
 import { changePrimary } from './scripts.js';
 
-await loadComponent('#header', '../components/header.html');
-await loadComponent('#footer', '../components/footer.html');
+await loadComponent('#header', '/components/header.html');
+await loadComponent('#footer', '/components/footer.html');
 
 toggleMenu('.navbar-toggle', '.navbar-collapse');
 toggleMenu('.colorz-toggle', '.colorz-picker');
